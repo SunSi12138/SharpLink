eec4e8f13225c984f72f4e7644234da439731729
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-08-21 16:30:52 +0800

    test(codec): require portable envelope schema

--- test/SharpLink.CodecCompatibility/PortableModels.cs
+++ test/SharpLink.CodecCompatibility/PortableModels.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace SharpLink.CodecCompatibility;
 
 internal sealed class CorpusEnvelope
 {
-    public int SchemaVersion { get; set; } = 1;
+    [JsonRequired]
+    public int SchemaVersion { get; set; }
     public RuntimeManifest Manifest { get; set; } = new();
     public Dictionary<string, string> CaseBytesBase64 { get; set; } = [];
 }
