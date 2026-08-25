46c1285d25b8445d26d050256e8ad590859b541b
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-08-25 23:31:03 +0800

    test: restore shared global usings (#333)

--- test/SharpLink.UnitTests/GlobalUsings.cs
+++ test/SharpLink.UnitTests/GlobalUsings.cs
@@ -2,7 +2,6 @@ global using System;
 global using System.Buffers;
 global using System.Collections.Generic;
 global using System.IO;
-global using System.Linq;
 global using SharpLink.Abstractions;
 global using SharpLink.Runtime;
 global using SharpLink.Serializer.SharpPack;
