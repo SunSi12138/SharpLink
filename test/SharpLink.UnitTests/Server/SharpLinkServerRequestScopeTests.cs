0d7d2ac830da78d00352dddb340efaf8e048cb3d
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-08-27 01:21:29 +0800

    chore(test): restore request-scope test formatting

--- test/SharpLink.UnitTests/Server/SharpLinkServerRequestScopeTests.cs
+++ test/SharpLink.UnitTests/Server/SharpLinkServerRequestScopeTests.cs
@@ -32,7 +32,7 @@ public class SharpLinkServerRequestScopeTests
         await Assert.That(syncUnary.DisposeCount).IsEqualTo(syncUnary.BeginCount);
         await Assert.That(syncUnary.MaxDepth).IsEqualTo(1);
         await Assert.That(asyncUnary.BeginCount).IsEqualTo(1);
-        await Assert.That(asyncUnary.DisposeCount).IsEqualTo(asyncUnary.BeginCount);
+        await Assert.That(asyncUnary.DisposeCount).IsEqualTo(syncUnary.BeginCount);
         await Assert.That(asyncUnary.MaxDepth).IsEqualTo(1);
         await Assert.That(syncOneWay.BeginCount).IsEqualTo(1);
         await Assert.That(syncOneWay.DisposeCount).IsEqualTo(syncOneWay.BeginCount);
@@ -399,6 +399,7 @@ public class SharpLinkServerRequestScopeTests
                 0,
                 null
             ])!;
+
         internal Task Observe(ValueTask dispatchTask, long requestId)
             => (Task)AwaitDispatchMethod.Invoke(Server, [dispatchTask, requestId])!;
 
