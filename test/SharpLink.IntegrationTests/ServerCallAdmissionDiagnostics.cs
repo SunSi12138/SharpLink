8492047c888265e11e98d6cab39547bc96f8e59a
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-01 11:09:57 +0800

    test(server): use server diagnostic accessors directly

--- test/SharpLink.IntegrationTests/ServerCallAdmissionDiagnostics.cs
+++ test/SharpLink.IntegrationTests/ServerCallAdmissionDiagnostics.cs
@@ -1,22 +1,10 @@
-using System.Reflection;
-
 namespace SharpLink.IntegrationTests;
 
 internal static class ServerCallAdmissionDiagnostics
 {
-    private const BindingFlags InstanceFlags =
-        BindingFlags.Instance | BindingFlags.NonPublic;
-
     internal static int ActiveCallCount(ISharpLinkServer server)
-        => GetCallAdmission(server).ActiveCallCount;
+        => ((SharpLinkServer)server).ActiveCallCountForDiagnostics;
 
     internal static int PendingCallAdmissions(ISharpLinkServer server)
-        => GetCallAdmission(server).PendingCallAdmissions;
-
-    private static ServerCallAdmission GetCallAdmission(ISharpLinkServer server)
-    {
-        ArgumentNullException.ThrowIfNull(server);
-        return server.GetType().GetField("_callAdmission", InstanceFlags)?.GetValue(server) as ServerCallAdmission ??
-            throw new InvalidOperationException("Server call-admission coordinator was not found.");
-    }
+        => ((SharpLinkServer)server).PendingCallAdmissionsForDiagnostics;
 }
