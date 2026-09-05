779de57b580b7719a3608e34970a4b86326aa4ca
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-05 20:47:03 +0800

    fix(generator): reuse existing type-name and identifier helpers

--- src/SharpLink.Generator/RpcGenerator.SharpPackIntegration.Helpers.cs
+++ src/SharpLink.Generator/RpcGenerator.SharpPackIntegration.Helpers.cs
@@ -5,9 +5,6 @@ public partial class RpcGenerator
     private static bool HasAttribute(ISymbol symbol, string ns, string name)
         => symbol.GetAttributes().Any(attribute => IsAttribute(attribute, ns, name));
 
-    private static string GetTypeName(ITypeSymbol type)
-        => type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-
     private static ITypeSymbol? GetMemberType(ISymbol member)
         => member switch
         {
@@ -15,10 +12,4 @@ public partial class RpcGenerator
             IPropertySymbol property => property.Type,
             _ => null
         };
-
-    private static string EscapeIdentifier(string identifier)
-        => Microsoft.CodeAnalysis.CSharp.SyntaxFacts.GetKeywordKind(identifier) !=
-           Microsoft.CodeAnalysis.CSharp.SyntaxKind.None
-            ? "@" + identifier
-            : identifier;
 }
