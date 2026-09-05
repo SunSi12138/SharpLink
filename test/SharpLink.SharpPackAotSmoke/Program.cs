3bf50066640a6a8e7f0e5b671ec67c98ad1566ee
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-05 21:55:00 +0800

    test: fix SharpPack AOT endpoint imports

--- test/SharpLink.SharpPackAotSmoke/Program.cs
+++ test/SharpLink.SharpPackAotSmoke/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using SharpLink.Abstractions;
 using SharpLink.AotExternalPayloads;
 using SharpLink.Client;
 using SharpLink.Runtime;
