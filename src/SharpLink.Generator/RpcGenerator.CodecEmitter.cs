82b3041e1db442d83339096a49c31a720aaa648c
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-06 02:07:37 +0800

    generator: emit native union codec family

--- src/SharpLink.Generator/RpcGenerator.CodecEmitter.cs
+++ src/SharpLink.Generator/RpcGenerator.CodecEmitter.cs
@@ -42,6 +42,8 @@ public partial class RpcGenerator
                 AppendAdapterCodecFactory(sb, codec);
             else if (codec.Kind == GeneratedCodecKind.Custom)
                 AppendCustomCodecFactory(sb, codec);
+            else if (codec.Kind == GeneratedCodecKind.Union)
+                AppendUnionCodec(sb, codec);
             else if (codec.Kind == GeneratedCodecKind.Dto)
             {
                 if (!dtoByCodecName.TryGetValue(codec.CodecName, out var dto))
