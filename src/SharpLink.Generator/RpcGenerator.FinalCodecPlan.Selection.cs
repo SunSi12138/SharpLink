8c817cda7fb7c54a806fb3017f1476c1500e71c2
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-22 01:12:25 +0800

    Move custom dispatch analysis out of selection file

--- src/SharpLink.Generator/RpcGenerator.FinalCodecPlan.Selection.cs
+++ src/SharpLink.Generator/RpcGenerator.FinalCodecPlan.Selection.cs
@@ -254,32 +254,6 @@ public partial class RpcGenerator
                     SupportsDirectCustomCodecDispatch(customCodec.CodecType, type)
             };
 
-        private static bool SupportsDirectCustomCodecDispatch(
-            INamedTypeSymbol codecType,
-            ITypeSymbol targetType)
-        {
-            var codecInterface = codecType.AllInterfaces.FirstOrDefault(item =>
-                item is
-                {
-                    Name: "IRpcCodec",
-                    Arity: 1
-                } &&
-                item.ContainingNamespace.ToDisplayString() == "SharpLink.Abstractions" &&
-                SymbolEqualityComparer.Default.Equals(item.TypeArguments[0], targetType));
-            if (codecInterface is null)
-                return false;
-
-            foreach (var member in codecInterface.GetMembers().OfType<IMethodSymbol>())
-            {
-                if (codecType.FindImplementationForInterfaceMember(member) is not IMethodSymbol implementation ||
-                    implementation.DeclaredAccessibility != Accessibility.Public)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
         private bool IsExternAliasOnlyImplementation(INamedTypeSymbol implementationType)
         {
             var assembly = implementationType.ContainingAssembly;
