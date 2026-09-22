ae2cde5808a9330b8d256a0b05b35e50edaa2656
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-23 01:21:27 +0800

    test(generator): align referenced fixtures with static Core ABI

--- test/SharpLink.Generator.Tests/RpcUnionCodecReferencedTests.cs
+++ test/SharpLink.Generator.Tests/RpcUnionCodecReferencedTests.cs
@@ -55,7 +55,7 @@ using SharpLink.Sdk;
     4,
     2,
     "2.0.0",
-    "sharplink-2.0-api4-rpcchannel-codec-provider-v1")]
+    "sharplink-2.0-api4-static-codec-core-v2")]
 
 namespace ReferencedUnion
 {
