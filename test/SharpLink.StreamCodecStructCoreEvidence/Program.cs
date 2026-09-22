b0f61ed82d04e78589dbb9d8215dce48d1115c72
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-22 14:07:03 +0800

    test: replay Phase C struct-core evidence for test/SharpLink.StreamCodecStructCoreEvidence/Program.cs

---
+++ test/SharpLink.StreamCodecStructCoreEvidence/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using SharpLink.StreamCodecStructCoreEvidence;
+
+#if GENERATED_BINDING_INTERFACE || GENERATED_BINDING_GUARDED_VALUE || GENERATED_BINDING_GUARDED_REF || GENERATED_BINDING_DIRECT
+GeneratedCodecBindingEvidenceRunner.Run(args);
+#else
+if (args.Length > 0 && string.Equals(args[0], "--generated-binding", StringComparison.Ordinal))
+    GeneratedCodecBindingEvidenceRunner.Run(args[1..]);
+else
+    StructCoreEvidenceRunner.Run(args);
+#endif
