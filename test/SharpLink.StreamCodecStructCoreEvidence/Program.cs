155d3143115519bacd7a40ef9616e5a301e04013
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-22 14:42:16 +0800

    test: route paired NativeAOT attribution mode

--- test/SharpLink.StreamCodecStructCoreEvidence/Program.cs
+++ test/SharpLink.StreamCodecStructCoreEvidence/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using SharpLink.StreamCodecStructCoreEvidence;
 
-#if GENERATED_BINDING_INTERFACE || GENERATED_BINDING_GUARDED_VALUE || GENERATED_BINDING_GUARDED_REF || GENERATED_BINDING_DIRECT
+#if GENERATED_BINDING_PAIRED
+GeneratedCodecBindingEvidenceRunner.RunAotPaired(args);
+#elif GENERATED_BINDING_INTERFACE || GENERATED_BINDING_GUARDED_VALUE || GENERATED_BINDING_GUARDED_REF || GENERATED_BINDING_DIRECT
 GeneratedCodecBindingEvidenceRunner.Run(args);
 #else
 if (args.Length > 0 && string.Equals(args[0], "--generated-binding", StringComparison.Ordinal))
