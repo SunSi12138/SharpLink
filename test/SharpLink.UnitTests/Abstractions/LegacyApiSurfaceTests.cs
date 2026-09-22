3908f51199456ae3e1a54d1ec696dc8412d7cfd5
SunSi12138 <54728594+SunSi12138@users.noreply.github.com>
2026-09-23 00:59:18 +0800

    test: include generic static codec dispatcher in raw inventory

--- test/SharpLink.UnitTests/Abstractions/LegacyApiSurfaceTests.cs
+++ test/SharpLink.UnitTests/Abstractions/LegacyApiSurfaceTests.cs
@@ -14,6 +14,7 @@ public class LegacyApiSurfaceTests
         "SharpLink.Runtime.IStreamDispatchLease",
         "SharpLink.Runtime.IStreamDispatchState",
         "SharpLink.Runtime.PooledAsyncStreamDispatcher`1",
+        "SharpLink.Runtime.PooledAsyncStreamDispatcher`2",
         "SharpLink.Runtime.PreAdmissionStreamDispatcher",
         "SharpLink.Runtime.DiscardingStreamDispatcher"
     ];
